a949a8d315e0e2cba1a0f6671bc67c97c8a8abca
Janos Erdos <janos.erdos@dbx.hu>
2018-08-17 17:53:50 +0200

    we have 276 converter functions!

--- src/convertible/coll.clj
+++ src/convertible/coll.clj
@@ -1,7 +1,21 @@
 (ns convertible.coll
-  (:import [java.util Collection List ArrayList Collections LinkedList])
+  (:import [java.util
+            Collection  Collections
+            List ArrayList LinkedList
+            Iterator ListIterator Spliterator
+            Set TreeSet HashSet])
   (:require [convertible.def :refer [converters defconv]]))
 
 (defconv Collection List (Collections/unmodifiableList (new ArrayList +input+)))
 (defconv Collection ArrayList (new ArrayList +input+))
 (defconv Collection LinkedList (new LinkedList +input+))
+
+(defconv Iterable Iterator (.iterator +input+))
+(defconv Iterable Spliterator (.spliterator +input+))
+
+(defconv List ListIterator (.listIterator +input+))
+
+(defconv Collection Set (Collections/unmodifiableSet (new TreeSet +input+)))
+
+(defconv Collection TreeSet (new TreeSet +input+))
+(defconv Collection HashSet (new HashSet +input+))
